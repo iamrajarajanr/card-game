37a7c20673a7c32857f08275a8994c2d44621d2d
RAJARAJAN R <iamrajarajanr@gmail.com>
2025-07-28 10:03:57 +0530

    changes

--- basics.js
+++ basics.js
@@ -1,38 +1,54 @@
-let firstcard = 5
-let secondcard = 5
+// let firstcard = 5
+// let secondcard = 5
 
-let value = firstcard + secondcard
+// let value = firstcard + secondcard
 
-console.log(value)
-
-
-
-let age = 18
-
-if (age > 18){
-    console.log("You are Eligible")
-}
-
-else if (age === 18){
-    console.log("Your age is perfectly match so you are eligible")
-}
-
-else{
-    console.log("You are not eligible")
-}
+// console.log(value)
 
 
-let personAge = 100
-console.log(personAge)
 
-if (personAge < 100){
-    console.log("Not eligible")
-}
+// let age = 18
 
-else if (personAge ===100){
-    console.log("Here is your birthday card from raj")
-}
+// if (age > 18){
+//     console.log("You are Eligible")
+// }
 
-else if (personAge > 100){
-    console.log("Not eligible. you already gotten one")
-}
+// else if (age === 18){
+//     console.log("Your age is perfectly match so you are eligible")
+// }
+
+// else{
+//     console.log("You are not eligible")
+// }
+
+
+// let personAge = 108
+// console.log(personAge)
+
+// if (personAge < 100){
+//     console.log("Not eligible")
+// }
+
+// else if (personAge ===100){
+//     console.log("Here is your birthday card from raj")
+// }
+
+// else {
+//     console.log("Not eligible. you already gotten one")
+// }
+
+
+// let sum = 20
+// console.log(sum)
+//  if (sum > 21){
+//     console.log("You are out of the game")
+//  }
+
+//  else if (sum === 21){
+//     console.log("You won blackjack")
+//  }
+
+//  else {
+//     console.log("You can get a new card")
+//  }
+
---
+++ script.js
@@ -0,0 +1,27 @@
+// let firstcard = 5
+// let secondcard = 5
+
+// let sum = firstcard + secondcard
+
+
+
+
+
+
+
+
+let sum = 20
+console.log(sum)
+
+
+ if (sum > 21){
+    console.log("You are out of the game")
+ }
+
+ else if (sum === 21){
+    console.log("You won blackjack")
+ }
+
+ else {
+    console.log("You can get a new card")
+ }
