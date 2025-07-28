4f2f2221b391d9b3906005c3810bde3cf2ffa155
RAJARAJAN R <iamrajarajanr@gmail.com>
2025-07-28 09:55:39 +0530

    basics

---
+++ basics.js
@@ -0,0 +1,38 @@
+let firstcard = 5
+let secondcard = 5
+
+let value = firstcard + secondcard
+
+console.log(value)
+
+
+
+let age = 18
+
+if (age > 18){
+    console.log("You are Eligible")
+}
+
+else if (age === 18){
+    console.log("Your age is perfectly match so you are eligible")
+}
+
+else{
+    console.log("You are not eligible")
+}
+
+
+let personAge = 100
+console.log(personAge)
+
+if (personAge < 100){
+    console.log("Not eligible")
+}
+
+else if (personAge ===100){
+    console.log("Here is your birthday card from raj")
+}
+
+else if (personAge > 100){
+    console.log("Not eligible. you already gotten one")
+}
